ab633a77bad5b77c6f44da4b27fa911862bc55c1
unknown <klarcalmeida@gmail.com>
2024-08-23 15:35:31 -0300

    Componente Medicamentos criado, tabela feita

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import Home from './components/Home.js'
-import Sobre from './components/Sobre.js'
-import Medicamnetos from './components/Medicamentos.js'
-import {BrowserRouter, Routes, Link, Route} from 'react-router-dom'
+import Home from './components/Home.js';
+import Sobre from './components/Sobre.js';
+import Medicamnetos from './components/Medicamentos.js';
+import {BrowserRouter, Routes, Link, Route} from 'react-router-dom';
+import {Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -10,17 +12,18 @@ function App() {
       <p>Gerenciador de Medicamentos</p>
 
       <BrowserRouter>
-        <ul>
-          <li> <link to="/">Pagina Inicial</link> </li>
-          <li> <link to="/medicamentos">Cadastro de Medicamentos</link></li>
-          <li> <link to="/sobre">Sobre</link></li>
-        </ul>
 
+        <Nav variant='tabs'>
+          <Nav.Link as={Link} to="/">Pagina Inicial</Nav.Link>
+          <Nav.Link as={Link} to="/medicamentos">Medicamentos</Nav.Link>
+          <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+        </Nav>
+  
         <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/medicamentos" element={<Medicamnetos />}></Route>
-        <Route path="/sobre" element={<Sobre />}></Route>
-      </Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/medicamentos" element={<Medicamnetos />}></Route>
+          <Route path="/sobre" element={<Sobre />}></Route>
+        </Routes>
 
       </BrowserRouter>
 
--- src/components/Medicamentos.js
+++ src/components/Medicamentos.js
@@ -1,9 +1,62 @@
-function Medicamentos(){
-    return(
-        <div>
-            <p>Esta é a pagina Medicamentos</p>
-        </div>
-    )
+import React from "react"
+import {Table} from "react-bootstrap"
+
+class Medicamentos extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            medicamentos : [
+                /** {"id":1, "nome": "Omeprazol", "data":"14/26"},
+                {"id":2, "nome": "Dipirona Monohidrata", "data":"14/25"},
+                {"id":3, "nome": "Cimegripe", "data":"08/26"},
+                {"id":4, "nome": "Benegripe", "data":"02/26"},
+                {"id":5, "nome": "Clonazepan", "data":"10/26"},
+                {"id":6, "nome": "Cafeína", "data":"20/27"}, */
+            ]
+        }
+    }
+
+    componentDidMount(){
+        fetch("https://localhost:3000/api/medicamentos")
+            .then(resposta => resposta.json())
+            .then(dados => {
+                this.setState({ medicamento : dados})
+            })
+    }
+
+    componentWillUnmount(){
+        alert("o componente medicamento foi desmontado!")
+    }
+
+    render(){
+        return(
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Validade</th>
+                        <th>Opções</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        this.state.medicamentos.map((medicamento) => //"medicamento" é variavel e "medicamentos" é o nome do state com os objetos json
+                            <tr>
+                                <td>{medicamento.id}</td>
+                                <td>{medicamento.nome}</td>
+                                <td>{medicamento.data}</td>
+                                <td>Atualizar Excluir</td>
+                            </tr>
+                        )
+                    }
+                    
+                </tbody>
+            </Table>
+        )
+    }    
 }
 
 export default Medicamentos;
